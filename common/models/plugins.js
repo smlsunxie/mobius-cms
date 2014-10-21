83f39c952a8cd77b10a2092e2b20ae3821e55b59
smlsunxie <smlsun@gmail.com>
2014-10-21 21:57:20 +0800

    finish install

--- common/models/plugins.js
+++ common/models/plugins.js
@@ -1,5 +1,18 @@
+var sys = require('sys')
+var exec = require('child_process').exec;
+puts = function(error, stdout, stderr) { sys.puts(stdout) }
+
+var module_home = "client/modules"
+
+
+
 module.exports = function(Plugins) {
   Plugins.install = function(url, name, cb) {
+    console.log("install command", "git clone "+url+" "+module_home+"/"+name);
+    exec("git clone "+url+" "+module_home+"/"+name, puts);
+
+
+
     console.log("url", url);
     console.log("name", name);
     cb(null, "install success!");
