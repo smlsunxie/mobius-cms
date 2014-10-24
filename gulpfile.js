7aeb1858a7e9060d6b951807bbf9e15e49d69eb0
smlsunxie <smlsun@gmail.com>
2014-10-24 18:11:26 +0800

    add client

---
+++ gulpfile.js
@@ -0,0 +1,170 @@
+'use strict';
+
+var gulp = require('gulp');
+var del = require('del');
+
+var path = require('path');
+
+var distPath = "client"
+
+
+// Load plugins
+var $ = require('gulp-load-plugins')();
+
+
+// Styles
+gulp.task('styles', function () {
+    return gulp.src('app/styles/main.scss')
+        .pipe($.rubySass({
+            style: 'expanded',
+            precision: 10,
+            loadPath: ['app/bower_components']
+        }))
+        .pipe($.autoprefixer('last 1 version'))
+        .pipe(gulp.dest(distPath+'/styles'))
+        .pipe($.size());
+});
+
+
+
+
+// CoffeeScript
+gulp.task('coffee', function () {
+    return gulp.src(
+            ['app/scripts/**/*.coffee', '!app/scripts/**/*.js'],
+            {base: 'app/scripts'}
+        )
+        .pipe(
+            $.coffee({ bare: true }).on('error', $.util.log)
+        )
+        .pipe(gulp.dest('app/scripts'));
+});
+
+
+// Scripts
+gulp.task('scripts', function () {
+    return gulp.src('app/scripts/app.js')
+        .pipe($.browserify({
+            insertGlobals: true,
+            transform: ['reactify']
+        }))
+        .pipe(gulp.dest(distPath+'/scripts'))
+        .pipe($.size());
+    });
+
+
+
+gulp.task('jade', function () {
+    return gulp.src('app/template/*.jade')
+        .pipe($.jade({ pretty: true }))
+        .pipe(gulp.dest(distPath+''));
+})
+
+
+
+// HTML
+gulp.task('html', function () {
+    return gulp.src('app/*.html')
+        .pipe($.useref())
+        .pipe(gulp.dest(distPath+''))
+        .pipe($.size());
+});
+
+// Images
+gulp.task('images', function () {
+    return gulp.src('app/images/**/*')
+        .pipe($.cache($.imagemin({
+            optimizationLevel: 3,
+            progressive: true,
+            interlaced: true
+        })))
+        .pipe(gulp.dest(distPath+'/images'))
+        .pipe($.size());
+});
+
+
+
+gulp.task('jest', function () {
+    var nodeModules = path.resolve('./node_modules');
+    return gulp.src('app/scripts/**/__tests__')
+        .pipe($.jest({
+            scriptPreprocessor: nodeModules + '/gulp-jest/preprocessor.js',
+            unmockedModulePathPatterns: [nodeModules + '/react']
+        }));
+});
+
+
+
+// Clean
+gulp.task('clean', function (cb) {
+    del([distPath+'/styles', distPath+'/scripts', distPath+'/images'], cb);
+});
+
+
+// Bundle
+gulp.task('bundle', ['styles', 'scripts', 'bower'], function(){
+    return gulp.src('./app/*.html')
+               .pipe($.useref.assets())
+               .pipe($.useref.restore())
+               .pipe($.useref())
+               .pipe(gulp.dest(distPath+''));
+});
+
+// Build
+gulp.task('build', ['html', 'bundle', 'images']);
+
+// Default task
+gulp.task('default', ['clean', 'build', 'jest' ]);
+
+// Webserver
+gulp.task('serve', function () {
+    gulp.src(distPath+'')
+        .pipe($.webserver({
+            livereload: true,
+            port: 9000
+        }));
+});
+
+// Bower helper
+gulp.task('bower', function() {
+    gulp.src('app/bower_components/**/*.js', {base: 'app/bower_components'})
+        .pipe(gulp.dest(distPath+'/bower_components/'));
+
+});
+
+gulp.task('json', function() {
+    gulp.src('app/scripts/json/**/*.json', {base: 'app/scripts'})
+        .pipe(gulp.dest(distPath+'/scripts/'));
+});
+
+
+// Watch
+gulp.task('watch', ['html', 'bundle', 'serve'], function () {
+
+    // Watch .json files
+    gulp.watch('app/scripts/**/*.json', ['json']);
+
+    // Watch .html files
+    gulp.watch('app/*.html', ['html']);
+
+
+    // Watch .scss files
+    gulp.watch('app/styles/**/*.scss', ['styles']);
+
+
+
+    // Watch .jade files
+    gulp.watch('app/template/**/*.jade', ['jade', 'html']);
+
+
+
+    // Watch .coffeescript files
+    gulp.watch('app/scripts/**/*.coffee', ['coffee', 'scripts', 'jest' ]);
+
+
+    // Watch .js files
+    gulp.watch('app/scripts/**/*.js', ['scripts', 'jest' ]);
+
+    // Watch image files
+    gulp.watch('app/images/**/*', ['images']);
+});
