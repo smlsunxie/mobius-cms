4143f76927bd955a076d67dec73bc8900306ad4a
smlsunxie <smlsun@gmail.com>
2014-10-29 22:32:14 +0800

    Compatibility with docker

--- client/lbclient/datasources.local.js
+++ client/lbclient/datasources.local.js
@@ -1,7 +1,12 @@
 var GLOBAL_CONFIG = require('../../global-config');
 
-module.exports = {
+var host = process.env.MOBIUS_1_PORT_3000_TCP_ADDR || "127.0.0.1"
+var port = process.env.MOBIUS_1_PORT_3000_TCP_PORT || "3000"
+
+var clientConfig = {
   remote: {
-    url: GLOBAL_CONFIG.restApiUrl
+    url: "http://"+host+":"+port+GLOBAL_CONFIG.restApiUrl
   }
-};
+}
+
+module.exports = clientConfig;
--- server/config.local.js
+++ server/config.local.js
@@ -2,7 +2,14 @@ var GLOBAL_CONFIG = require('../global-config');
 
 var isDevEnv = (process.env.NODE_ENV || 'development') === 'development';
 
+var host = process.env.MOBIUS_1_PORT_3000_TCP_ADDR || "127.0.0.1"
+var port = process.env.MOBIUS_1_PORT_3000_TCP_PORT || "3000"
+
+
+
 module.exports = {
+  host: host,
+  port: port,
   restApiRoot: GLOBAL_CONFIG.restApiRoot,
   livereload: process.env.LIVE_RELOAD,
   isDevEnv: isDevEnv
