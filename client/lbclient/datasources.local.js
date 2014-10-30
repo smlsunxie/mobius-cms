16718a54e17b20086a170d79bb4c3a49c1d31e51
smlsunxie <smlsun@gmail.com>
2014-10-30 16:58:56 +0800

    add log

--- client/lbclient/datasources.local.js
+++ client/lbclient/datasources.local.js
@@ -3,6 +3,8 @@ var GLOBAL_CONFIG = require('../../global-config');
 var host = process.env.REMOTE_HOST || "127.0.0.1"
 var port = process.env.REMOTE_PORT || "3000"
 
+console.log("remote url:", "http://"+host+":"+port+GLOBAL_CONFIG.restApiUrl);
+
 var clientConfig = {
   remote: {
     url: "http://"+host+":"+port+GLOBAL_CONFIG.restApiUrl
